3bb99621357ecaa94f45407c4f7686beb2631fdc
Sarun Dhoju <sarundhoju775@gmail.com>
2025-12-11 16:10:40 +0545

    quotes added

---
+++ quote.js
@@ -0,0 +1,48 @@
+const quote = {
+  science: [
+    {
+      text: "The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom.",
+      author: "Isaac Asimov",
+    },
+    {
+      text: "Never memorize something that you can look up.",
+      author: "Albert Einstein",
+    },
+    {
+      text: "Everything must be made as simple as possible. But not simpler.",
+      author: "Albert Einstein",
+    },
+    {
+      text: "If I have seen further it is by standing on the shoulders of Giants.",
+      author: "Isaac Newton",
+    },
+  ],
+  computer: [
+    {
+      text: "Doing research on the Web is like using a library assembled piecemeal by pack rats and vandalized nightly.",
+      author: "Roger Ebert",
+    },
+    {
+      text: "That's the thing about people who think they hate computers. What they really hate is lousy programmers.",
+      author: "Larry Niven",
+    },
+    {
+      text: "Computers are like Old Testament gods; lots of rules and no mercy.",
+      author: "Joseph Campbell",
+    },
+  ],
+  motivational: [
+    {
+      text: "If you want to do this, if you want to play big, if you want to really impact lives, you’ve got to face yourself. You’ve got to be courageous and willing to go all in and address everything about you that is uncomfortable.",
+      author: " Harry Lopez",
+    },
+    {
+      text: "We should care as much about well-doing as well-being. I want to live in a world that values purpose as much as pleasure, contribution as much as contentment, honesty as much as excitement, and justice as much as joy.",
+      author: "Adam Grant",
+    },
+    {
+      text: "Too many people spend money they earned to buy things they don’t want to impress people that they don’t like.",
+      author: "Will Rogers",
+    },
+  ],
+};
